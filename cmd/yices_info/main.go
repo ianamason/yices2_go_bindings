848a2c891dc176a2579bdf11e79fb2159ae30355
Ian A.Mason <iam.csl.sri.com>
2019-10-09 13:30:34 -0700

    Zero content, but at least I get an A.

--- cmd/yices_info/main.go
+++ cmd/yices_info/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"github.com/ianamason/yices2_go_bindings/yices2"
 	"fmt"
+	"github.com/ianamason/yices2_go_bindings/yices2"
 )
-
 
 func main() {
 
--- yices2/yices.go
+++ yices2/yices.go
@@ -1,9 +1,9 @@
 package yices
+
 // #cgo CFLAGS: -g -fPIC
 // #cgo LDFLAGS:  -lyices -lgmp
 // #include <yices.h>
 import "C"
-
 
 func version() string {
 	return "just little steps for now"
