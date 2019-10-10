8562d6d2ac691f7f1717ff28d39ec4eaa68cd135
Ian A.Mason <iam.csl.sri.com>
2019-10-10 16:00:13 -0700

    9% of yices.h

--- yices2/yices_api.go
+++ yices2/yices_api.go
@@ -80,3 +80,96 @@ func Print_error(f *os.File) int32 { return int32(C.yices_print_error_fd(C.int(f
 func Error_string() string {
 	return C.GoString(C.yices_error_string())
 }
+
+/********************************
+ *  VECTORS OF TERMS AND TYPES  *
+ *******************************/
+
+//iam: probably not going to export these 
+//__YICES_DLLSPEC__ extern void yices_init_term_vector(term_vector_t *v);
+//__YICES_DLLSPEC__ extern void yices_init_type_vector(type_vector_t *v);
+//__YICES_DLLSPEC__ extern void yices_delete_term_vector(term_vector_t *v);
+//__YICES_DLLSPEC__ extern void yices_delete_type_vector(type_vector_t *v);
+//__YICES_DLLSPEC__ extern void yices_reset_term_vector(term_vector_t *v);
+//__YICES_DLLSPEC__ extern void yices_reset_type_vector(type_vector_t *v);
+
+
+/***********************
+ *  TYPE CONSTRUCTORS  *
+ **********************/
+
+//iam: we use a type definition, does it improve readbility?
+
+type Type_t int32 
+
+func Bool_type() Type_t { return Type_t(C.yices_bool_type()) }
+
+func Int_type() Type_t { return Type_t(C.yices_int_type()) }
+
+func Real_type() Type_t { return Type_t(C.yices_real_type()) }
+
+func Bv_type(size uint32) Type_t  { return Type_t(C.yices_bv_type(C.uint32_t(size))) }
+
+func New_scalar_type(card uint32) Type_t  { return Type_t(C.yices_new_scalar_type(C.uint32_t(card))) }
+
+func New_uninterpreted_type() Type_t { return Type_t(C.yices_new_uninterpreted_type()) }
+
+/*
+//iam: icky
+func type_slice_to_C_array(s []Type_t) *C.int {
+	var carr = make([]C.int, len(s), len(s))
+	for i := 0; i < len(s); i++ {
+		carr[i] = C.int(s[i])
+	}
+	return &carr[0]
+}
+func Tuple_type(tau []Type_t) Type_t {
+	tau_len := len(tau)
+	return Type_t(C.yices_tuple_type(C.uint32_t(tau_len), type_slice_to_C_array(tau)))
+}
+*/
+
+func Tuple_type(tau []Type_t) Type_t {
+	tau_len := len(tau)
+	return Type_t(C.yices_tuple_type(C.uint32_t(tau_len), (*C.int32_t)(&tau[0])))
+}
+
+func Tuple_type1(tau1 Type_t) Type_t {
+	carr := []C.int32_t{ C.int32_t(tau1) }
+	return Type_t(C.yices_tuple_type(C.uint32_t(1), (*C.int32_t)(&carr[0])))
+}
+
+func Tuple_type2(tau1 Type_t, tau2 Type_t) Type_t {
+	carr := []C.int32_t{ C.int32_t(tau1), C.int32_t(tau2) }
+	return Type_t(C.yices_tuple_type(C.uint32_t(2), (*C.int32_t)(&carr[0])))
+}
+
+func Tuple_type3(tau1 Type_t, tau2 Type_t, tau3 Type_t) Type_t {
+	carr := []C.int32_t{ C.int32_t(tau1), C.int32_t(tau2), C.int32_t(tau3) }
+	return Type_t(C.yices_tuple_type(C.uint32_t(3), (*C.int32_t)(&carr[0])))
+}
+
+
+func Function_type(dom []Type_t, rng Type_t) Type_t {
+	dom_len := len(dom)
+	return Type_t(C.yices_function_type(C.uint32_t(dom_len), (*C.int32_t)(&dom[0]), C.int32_t(rng)))
+}
+
+func Function_type1(tau1 Type_t, rng Type_t) Type_t {
+	carr := []C.int32_t{ C.int32_t(tau1) }
+	return Type_t(C.yices_function_type(C.uint32_t(1), (*C.int32_t)(&carr[0]), C.int32_t(rng)))
+}
+
+func Function_type2(tau1 Type_t, tau2 Type_t, rng Type_t) Type_t {
+	carr := []C.int32_t{ C.int32_t(tau1), C.int32_t(tau2) }
+	return Type_t(C.yices_function_type(C.uint32_t(2), (*C.int32_t)(&carr[0]), C.int32_t(rng)))
+}
+
+func Function_type3(tau1 Type_t, tau2 Type_t, tau3 Type_t, rng Type_t) Type_t {
+	carr := []C.int32_t{ C.int32_t(tau1), C.int32_t(tau2), C.int32_t(tau3) }
+	return Type_t(C.yices_function_type(C.uint32_t(3), (*C.int32_t)(&carr[0]), C.int32_t(rng)))
+}
+
+/*************************
+ *   TYPE EXPLORATION    *
+ ************************/
