2ae455c77e5228eca519aa39c5c6addce4a976d5
Ian A. Mason <iam@csl.sri.com>
2019-11-12 08:10:31 -0800

    testing the new layer

--- cmd/yices_info/main.go
+++ cmd/yices_info/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	yapi "github.com/ianamason/yices2_go_bindings/yices_api"
+	"github.com/ianamason/yices2_go_bindings/yices"
 )
 
 func main() {
 
-	fmt.Printf("Yices version no:     %s\n", yapi.Version())
-	fmt.Printf("Yices build arch:     %s\n", yapi.Build_arch())
-	fmt.Printf("Yices build mode:     %s\n", yapi.Build_mode())
-	fmt.Printf("Yices build date:     %s\n", yapi.Build_date())
-	fmt.Printf("Yices has mcsat:      %d\n", yapi.Has_mcsat())
-	fmt.Printf("Yices is thread safe: %d\n", yapi.Is_thread_safe())
+	fmt.Printf("Yices version no:     %s\n", yices.Version)
+	fmt.Printf("Yices build arch:     %s\n", yices.Build_arch)
+	fmt.Printf("Yices build mode:     %s\n", yices.Build_mode)
+	fmt.Printf("Yices build date:     %s\n", yices.Build_date)
+	fmt.Printf("Yices has mcsat:      %d\n", yices.Has_mcsat)
+	fmt.Printf("Yices is thread safe: %d\n", yices.Is_thread_safe)
 
 }
