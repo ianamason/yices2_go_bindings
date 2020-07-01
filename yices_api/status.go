7e0c4f334c398876b80c8b111f3d74cb7959226b
Ian A.Mason <iam@csl.sri.com>
2020-07-01 10:04:38 -0700

    status.go gets comments that make golint happy.

--- yices_api/status.go
+++ yices_api/status.go
@@ -1,7 +1,9 @@
 package yices2
 
+// SmtStatusT is the analog of smt_status_t defined in yices_types.h
 type SmtStatusT int32
 
+// These are the analogs to the elements of smt_status_t defined in yices_types.h
 const (
 	StatusIdle SmtStatusT = iota
 	StatusSearching
